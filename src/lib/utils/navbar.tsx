34a07c81f6ec008393af00ffeaf0984986932a9d
Gaëtan H <gaetanhus@gmail.com>
2023-12-10 21:02:11 +0100

    Refactor navbar component and add random icon on hover

--- src/lib/utils/navbar.tsx
+++ src/lib/utils/navbar.tsx
@@ -1,70 +1,59 @@
 "use client";
 
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, NavbarMenuToggle, NavbarMenu, NavbarMenuItem } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link as NLink, Button, NavbarMenuToggle,
+  NavbarMenu, NavbarMenuItem } from "@nextui-org/react";
+import Link from "next/link";
 import type { ReactElement } from "react";
-import { useState } from "react";
+import { IconPlayFootball, IconPlayBasketball, IconPlayVolleyball, IconSkateboarding, IconYoga, IconWaterpolo, IconWalk,
+  IconTrekking, IconTreadmill, IconRun, IconShiJumping } from "@tabler/icons-react";
+import { useRef, useState } from "react";
+import { useHover } from "usehooks-ts";
 
-export const AcmeNavbar = (): ReactElement => {
+
+const random = (): ReactElement => {
+  const icons = [<IconPlayFootball key={1} size={24} />, <IconPlayBasketball key={2} size={24} />,
+    <IconPlayVolleyball key={3} size={24} />, <IconSkateboarding key={4} size={24} />,
+    <IconYoga key={5} size={24} />, <IconWaterpolo key={6} size={24} />,
+    <IconWalk key={7} size={24} />, <IconTrekking key={8} size={24} />,
+    <IconTreadmill key={9} size={24} />, <IconRun key={10} size={24} />,
+    <IconShiJumping key={11} size={24} />];
+  return icons[Math.floor(Math.random() * icons.length)];
+};
+
+export const SuperMegaCooooolNavbar = (): ReactElement => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const hoverRef = useRef(null);
+  const isHover = useHover(hoverRef);
 
-  const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out"
-  ];
+  const menuItems = ["Features", "Pricing"];
 
   return (
-    <Navbar onMenuOpenChange={setIsMenuOpen}>
+    <Navbar isBlurred onMenuOpenChange={setIsMenuOpen}>
       <NavbarContent>
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
           className="sm:hidden"
         />
         <NavbarBrand>
-          <p className="font-bold text-inherit">ACME</p>
+          <Link href="/" className="flex items-center gap-2">
+            <span ref={hoverRef}>{isHover ? random() : random()}</span>
+            Lose
+          </Link>
         </NavbarBrand>
       </NavbarContent>
-
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
+          <Button as={Link} color="secondary" href="/sign-in" variant="flat">
+            Get started
           </Button>
         </NavbarItem>
       </NavbarContent>
       <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
-            <Link color={ index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"} className="w-full" href="#" size="lg" >
+            <NLink color={ index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"} className="w-full" href="#" size="lg" >
               {item}
-            </Link>
+            </NLink>
           </NavbarMenuItem>
         ))}
       </NavbarMenu>
