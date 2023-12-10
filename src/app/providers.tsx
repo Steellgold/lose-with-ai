c0091c5619c98c509567e4f8513e1065d58c895d
Gaëtan H <gaetanhus@gmail.com>
2023-12-10 21:01:58 +0100

    Add UserProvider and Toaster to Providers component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,18 @@
 // app/providers.tsx
 "use client";
 
+import { UserProvider } from "@/lib/providers";
 import { NextUIProvider } from "@nextui-org/react";
 import type { PropsWithChildren, ReactElement } from "react";
+import { Toaster } from "sonner";
 
 export const Providers = ({ children }: PropsWithChildren): ReactElement => {
   return (
     <NextUIProvider>
-      {children}
+      <UserProvider>
+        <Toaster />
+        {children}
+      </UserProvider>
     </NextUIProvider>
   );
 };
