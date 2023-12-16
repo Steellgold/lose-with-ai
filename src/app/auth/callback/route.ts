080bd54e21e363f453dd83df85b4884effbc246a
Gaëtan H <gaetanhus@gmail.com>
2023-12-16 14:08:50 +0100

    Add user creation logic in callback route

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -2,6 +2,8 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { createClient } from "@/lib/utils/supabase/server";
+import { prisma } from "@/lib/utils/db/prisma";
+import { getAMail, getAName } from "@/lib/utils/user-utils";
 
 export const GET = async(request: NextRequest): Promise<NextResponse> => {
   const requestUrl = new URL(request.url);
@@ -11,6 +13,19 @@ export const GET = async(request: NextRequest): Promise<NextResponse> => {
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
     await supabase.auth.exchangeCodeForSession(code);
+
+    const u = await supabase.auth.getUser();
+
+    const user = await prisma.user.findUnique({ where: { email: u.data.user?.email } });
+    if (!user) {
+      await prisma.user.create({
+        data: {
+          email: getAMail(u.data.user),
+          credit: 2,
+          name: getAName(u.data.user)
+        },
+      });
+    }
   }
 
   return NextResponse.redirect(requestUrl.origin);
