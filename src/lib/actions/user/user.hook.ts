8a31f416d0d287d1f562ab56e32927f27e43db09
Gaëtan H <gaetanhus@gmail.com>
2023-12-17 13:30:07 +0100

    Add user hook for querying and mutating user data

---
+++ src/lib/actions/user/user.hook.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import type { UseQueryOptions } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { deleteUser, updateUser, getUser } from ".";
+import type { Prisma } from "@prisma/client";
+
+// Queries
+export const getUserQuery = (userId: string) => {
+  return { queryKey: ["getUser"], queryFn: () => getUser(userId) };
+}
+
+export const useGetUser = (userId: string) => {
+  return useQuery(getUserQuery(userId));
+}
+
+// Mutations
+export const updateUserMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (user: Prisma.UserUpdateInput) => updateUser(user),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["getUser"] }),
+  });
+}
+
+export const deleteUserMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (userId: string) => deleteUser(userId),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["getUser"] }),
+  });
+}
